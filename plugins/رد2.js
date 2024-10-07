e316c88fb089334eec26615131fbc1427f81953f
marwangt <162702741+marwangt@users.noreply.github.com>
2024-10-07 13:39:13 +0100

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
-await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-    conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
+    let message = '*`ريتا تحت امرك 🫡💗 لي طلب الاوامر اكتب (.اوامر) 🌸`*';
+await conn.sendMessage(m.chat, { react: { text: '🌸', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/GLhbA.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^بوت$/i;
