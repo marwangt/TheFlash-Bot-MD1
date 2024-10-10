71c158215a24b909119c5e1f38dd9ecfd9ff2100
marwangt <162702741+marwangt@users.noreply.github.com>
2024-10-10 09:49:15 +0100

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*`ريتا تحت امرك 🫡💗 لي طلب الاوامر اكتب (.اوامر) 🌸`*';
-await conn.sendMessage(m.chat, { react: { text: '🌸', key: m.key } })
+    let message = '*`مرحبا @user انا ريتا ☺️🍉 لي طلب الاوامر اكتب (.اوامر) 🌸`*';
+await conn.sendMessage(m.chat, { react: { text: '🍇', key: m.key } })
     conn.sendFile(m.chat, 'https://qu.ax/GLhbA.jpg', 'image.jpg', message, m);
 };
 
